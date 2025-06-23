7419899c6fd3a20b713c37ceba0016d638fc18cf
tmcgahey93 <tmcgahey93@gmail.com>
2025-06-23 19:13:49 -0400

    Remove Test Code

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
